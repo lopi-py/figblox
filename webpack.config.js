d7b96b8ee6c21f618f3a84e4d16629699a895b97
lopy <70210066+lopi-py@users.noreply.github.com>
2023-07-20 01:20:54 -0500

    chore: update webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const path = require("path");
 
 module.exports = {
   entry: "./out/index.js",
+  mode: "development",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'code.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "code.js",
   },
 };
